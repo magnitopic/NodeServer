55719f1ae4376d43bfa053bf524e3f19bf4fd6f8
magnitopic <magnitopic@gmail.com>
2020-12-18 22:52:56 +0100

    Tini experiments and first steps on the dice page

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
 
   let filePath = path.join(
     __dirname,
-    "public",
+    "web",
     req.url === "/" ? "index.html" : req.url
   );
 
@@ -47,7 +47,7 @@ const server = http.createServer((req, res) => {
       if (err.code == "ENOENT") {
         // Page not found
         fs.readFile(
-          path.join(__dirname, "public", "404.html"),
+          path.join(__dirname, "web","404.html"),
           (err, content) => {
             res.writeHead(404, { "Content-Type": "text/html" });
             res.end(content, "utf8");
