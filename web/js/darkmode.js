95711dbe09de66e14e5cee1e4865fac03d6d0d32
magnitopic <magnitopic@gmail.com>
2020-12-27 20:38:39 +0100

    Started using sass

---
+++ web/js/darkmode.js
@@ -0,0 +1,8 @@
+localStorage.setItem('darkMode', "true");
+const a= localStorage.getItem('darkMode');
+if(a=='true'){
+} else if (a='false'){
+    
+}else{
+    localStorage.setItem('darkMode', "false");
+}
