4b833d030afa3a834eea7fd5f70a4d8762eaf53a
magnitopic <magnitopic@gmail.com>
2021-01-04 23:29:00 +0100

    Firsts steps with routes

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const path= require('path');
 const mongoose=require('mongoose');
 const mongo=require('./connection.js');
 const members= require('./members.js');
+const authRoute=require('./routes/auth'); 
 const app=express();
 mongo.a();
+
+app.use('/api/user', authRoute)
+
 
 app.use(express.static(path.join(__dirname, 'web')))
 
 //Da un JSON como respuesta a una petición GET
-app.get('/api/members',(req,res)=>res.json(members))
+app.get('/api',(req,res)=>res.json(members))
 
 
 const PORT=process.env.PORT || 5000;
