ccfd6ef8244319da8cc453129b5254b0bdab26ac
magnitopic <magnitopic@gmail.com>
2021-01-06 22:23:07 +0100

    darkmode code is now in darkmode.js

--- web/js/darkmode.js
+++ web/js/darkmode.js
@@ -1,13 +1,28 @@
-localStorage.setItem('darkMode', "true");
-const a= localStorage.getItem('darkMode');
-if(a=='true'){
+const check =document.getElementById("check")
 
-} else if (a='false'){
-    
-}else{
+if (localStorage.getItem('darkMode')===null){
     localStorage.setItem('darkMode', "false");
 }
+var a= localStorage.getItem('darkMode');
 
-function cambiarColor(){
-    
+checkStatus()
+
+function checkStatus(){
+    if (localStorage.getItem('darkMode')==="true"){
+        check.checked = true;
+        document.body.style.backgroundColor = "#181A1B";
+    }else{
+        check.checked = false;
+        document.body.style.backgroundColor = "#FFF";
+    }
+}
+
+function changeStatus(){
+    if (localStorage.getItem('darkMode')==="true"){
+        localStorage.setItem('darkMode', "false");
+        document.body.style.backgroundColor = "#FFF";
+    } else{
+        localStorage.setItem('darkMode', "true");
+        document.body.style.backgroundColor = "#181A1B";
+    }
 }
