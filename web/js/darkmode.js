5fb062a1dd5e25d0acdc3b3eba61f43e4bec4f7c
magnitopic <magnitopic@gmail.com>
2021-01-08 16:05:59 +0100

    Enhanced dark mode

--- web/js/darkmode.js
+++ web/js/darkmode.js
@@ -3,16 +3,17 @@ const check =document.getElementById("check")
 if (localStorage.getItem('darkMode')===null){
     localStorage.setItem('darkMode', "false");
 }
-var a= localStorage.getItem('darkMode');
 
 checkStatus()
 
 function checkStatus(){
     if (localStorage.getItem('darkMode')==="true"){
         check.checked = true;
+        document.getElementsByTagName("p")[0].style.color="#FFF";
         document.body.style.backgroundColor = "#181A1B";
     }else{
         check.checked = false;
+        document.getElementsByTagName("p")[0].style.color="black";
         document.body.style.backgroundColor = "#FFF";
     }
 }
@@ -20,9 +21,11 @@ function checkStatus(){
 function changeStatus(){
     if (localStorage.getItem('darkMode')==="true"){
         localStorage.setItem('darkMode', "false");
+        document.getElementsByTagName("p")[0].style.color="black";
         document.body.style.backgroundColor = "#FFF";
     } else{
         localStorage.setItem('darkMode', "true");
+        document.getElementsByTagName("p")[0].style.color="#FFF";
         document.body.style.backgroundColor = "#181A1B";
     }
 }
